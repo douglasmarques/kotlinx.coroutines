5a9636ad67e28df1138cd4cb68d5a9276e1c8fe7
Nikita Koval <nikita.koval@jetbrains.com>
2022-01-19 23:56:25 +0600

    Unignore some tests

--- kotlinx-coroutines-core/jvm/test/channels/ChannelUndeliveredElementSelectOldStressTest.kt
+++ kotlinx-coroutines-core/jvm/test/channels/ChannelUndeliveredElementSelectOldStressTest.kt
@@ -8,7 +8,6 @@ import kotlinx.atomicfu.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.selects.*
 import org.junit.After
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.*
 import org.junit.runners.*
@@ -19,7 +18,6 @@ import kotlin.test.*
  * Tests resource transfer via channel send & receive operations, including their select versions,
  * using `onUndeliveredElement` to detect lost resources and close them properly.
  */
-//@Ignore
 @RunWith(Parameterized::class)
 class ChannelUndeliveredElementSelectOldStressTest(private val kind: TestChannelKind) : TestBase() {
     companion object {
